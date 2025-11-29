47d946c88d105ae693c7750ec21c9420b6977767
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-11-29 19:58:33 +0530

    person is eligible for vote casting or not

---
+++ Eligibility_for_vote.c
@@ -0,0 +1,17 @@
+// Check a person is eligible for vote casting or not.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    int age;
+    printf("Enter your Age: \n");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("you are eligible to cast your vote");
+    }
+    printf("Thanks you for being a good citizen!");
+}
