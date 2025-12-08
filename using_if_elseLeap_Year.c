7c065731c17f6169926b75eb97fc94175cbcbb0e
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-08 20:57:00 +0530

    By using if_else

---
+++ using_if_elseLeap_Year.c
@@ -0,0 +1,22 @@
+/*Write a program that checks if a given year is a leap year or not, outputs the result in a specific format, and handles invalid years as well.*/
+
+#include <stdio.h>
+
+int main() {
+
+    int year;
+    scanf("%d",&year);
+    if(year<=0)
+    {
+        printf("The given year %d is Invalid year.",year);
+    }
+    else if(year % 4 == 0 && year %100!=0 || year%400==0)
+    {
+        printf("Yes, %d is a leap year.",year);
+    }
+    else
+    {
+        printf("No, %d is not a leap year.",year); 
+    }
+    return 0;
+}
