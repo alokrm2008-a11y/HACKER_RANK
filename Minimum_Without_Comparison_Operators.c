391a99ede5d604d92499a93c1549b8eb393b016c
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-19 01:02:59 +0530

    Bitwise_Minimum Without Comparison Operators

---
+++ Minimum_Without_Comparison_Operators.c
@@ -0,0 +1,24 @@
+/*Write a program to find the minimum of two integers a and b without using any comparison operators (<, >, <=, >=). Use only bitwise operators and logical operators to determine which number is smaller.
+
+Input Format
+
+Two space-separated integers a and b.
+Constraints
+
+−10^9 ≤ a,b ≤ 10^9
+Output Format
+
+Print the smaller of the two integers.*/
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int min = b ^ ((a ^ b) & -(a < b));
+
+    printf("%d", min);
+    return 0;
+}
