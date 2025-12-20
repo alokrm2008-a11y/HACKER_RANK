8a08c5955cff7569077203de3fa4027aaf57974d
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-21 00:13:54 +0530

    Rectangle area calculation
    Added instructions for calculating rectangle area using a separate function.

--- Area_of_a_Rectangle.c
+++ Area_of_a_Rectangle.c
@@ -1,5 +1,23 @@
 /*You are given the length and width of a rectangle.
-Your task is to calculate and print its area using a separate function, not inside main().*/
+Your task is to calculate and print its area using a separate function, not inside main().
+
+Important Instructions
+
+Do NOT write the entire logic in main().
+Create a separate function int area(int length, int width) and call it from main().
+Input Format
+
+Two integers: {length} {width}
+
+Constraints
+
+1 ≤ length ≤ 10⁶
+1 ≤ width ≤ 10⁶
+Output Format
+
+Single line printing the result as:
+
+The area is: {res} units*/
 
 #include <stdio.h>
 #include <string.h>
