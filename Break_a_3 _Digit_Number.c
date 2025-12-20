f789922044eb37fdef958052e7b1ff23d6f29146
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-21 00:20:02 +0530

    Enhance for 3-digit number extraction

--- Break_a_3 _Digit_Number.c
+++ Break_a_3 _Digit_Number.c
@@ -1,4 +1,19 @@
-/*Given a 3-digit number ABC, extract and print A, B, and C separately.*/
+/*Given a 3-digit number ABC, extract and print A, B, and C separately.
+
+Important Instructions
+
+Do NOT write the entire logic in main().
+Create a function void split(int n) and call it from main().
+Input Format
+
+A single 3-digit integer {ABC}
+
+Constraints
+
+100 ≤ n ≤ 999
+Output Format
+
+Single line printing the output as: {A} {B} {C}*/
 
 #include <stdio.h>
 #include <string.h>
