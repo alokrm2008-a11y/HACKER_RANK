ac80ae0ad7a44a92991fe87a14f775dc245789a9
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-22 21:57:56 +0530

    Sum Last Digits

--- Sum_Last_Digits.c
+++ Sum_Last_Digits.c
@@ -1,4 +1,21 @@
-//Given two integers, extract their last digits and print the sum of those digits.
+/*Given two integers, extract their last digits and print the sum of those digits.
+
+Important Instructions
+
+Do NOT write the entire logic in main().
+Write a function int sumLastDigits(int a, int b) and call it from main().
+Input Format
+
+Two integers: {a} {b}
+
+Constraints
+
+0 ≤ a, b ≤ 10⁹
+Output Format
+
+Single line printing the result as: The sum of last digits is: {res}*/
+
+
 
 #include <stdio.h>
 #include <string.h>
