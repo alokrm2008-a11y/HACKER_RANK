781a2117aee89b4f1b9e44ec76e1559952845470
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-23 19:41:13 +0530

    Update Print_All_Factors_of_a_Number.c

--- Print_All_Factors_of_a_Number.c
+++ Print_All_Factors_of_a_Number.c
@@ -1,3 +1,19 @@
+/*Given a positive integer N, print all its factors in ascending order.
+
+You need to write a program that reads N and prints all its factors separated by spaces.
+
+Input Format
+
+A single line containing the integer N.
+
+Constraints
+
+1 ≤ 𝑁 ≤ 10^9
+Output Format
+
+Print all factors of N in ascending order, separated by a single space.*/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
