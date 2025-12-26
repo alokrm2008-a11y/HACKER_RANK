261c70292471e29f86ae72523bf1f528fcd46183
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-27 03:05:17 +0530

    Add files via upload

---
+++ Arithmetic_Progression.c
@@ -0,0 +1,19 @@
+
+
+
+
+#include <stdio.h>
+
+int main() {
+
+    int n,a,d;
+    scanf("%d", &n);
+    scanf("%d %d", &a, &d);
+    
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ",a);
+        a = a + d;
+    }
+    return 0;
+}
