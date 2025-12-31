9482a073ba6f66460b51953bf7f4db4d67706edb
Alok Kumar Mahato <alokrm2008@gmail.com>
2025-12-31 20:53:19 +0530

    finds numbers divisible by 3 and 5
    Added a comment explaining the program's purpose.

--- Divisible_by 3_and 5.c
+++ Divisible_by 3_and 5.c
@@ -1,5 +1,6 @@
 /*Write a program that prints all the numbers between 1 and 100 (inclusive) that are divisible by both 3 and 5.*/
 
+----------------------------------------------------------------------[Answer]--------------------------------------------------------------------------------------------
 
 #include <stdio.h>
 
