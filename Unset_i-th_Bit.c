140f524f864288659c70175703844468e09bc786
Alok Kumar Mahato <alokrm2008@gmail.com>
2026-01-02 22:03:30 +0530

    Update Unset_i-th_Bit.c

--- Unset_i-th_Bit.c
+++ Unset_i-th_Bit.c
@@ -1,3 +1,17 @@
+/*Given a number n and a position i, write a program to unset the i-th bit of n using bitwise operators.
+
+Input Format
+
+The first line contains an integer n.
+The second line contains an integer i.
+Constraints
+
+-10^9 ≤ n ≤ 10^9
+0 ≤ i ≤ 31
+Output Format
+
+Print the updated value of n after unsetting the i-th bit.*/
+
 
 
 #include <stdio.h>
