a98818fa1645502a2eefa3dfb29ffd1d0985b6bd
Alok Kumar Mahato <alokrm2008@gmail.com>
2026-01-03 19:47:51 +0530

    Update Staircase.c

--- Staircase.c
+++ Staircase.c
@@ -1,4 +1,31 @@
+/*Staircase detail
 
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the  function with the following parameter(s):
+
+: an integer
+Print
+
+Print a staircase as described above. No value should be returned.
+Note: The last line is not preceded by spaces. All lines are right-aligned.
+
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Constraints
+0<n<=100.*/
 
 
 #include <stdio.h>
