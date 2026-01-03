aef0b4ae6b1cb0c2018d04878b32391d72fdfef3
Alok Kumar Mahato <alokrm2008@gmail.com>
2026-01-03 19:49:48 +0530

    Pattern printing Staircase

--- Staircase.c
+++ Staircase.c
@@ -4,7 +4,7 @@ This is a staircase of size :
 
    #
   ##
- ###
+ ### 
 ####
 Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 
